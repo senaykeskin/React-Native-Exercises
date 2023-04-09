63566c9358511e1e4ba656115a09068286dc2951
senaykeskin <senay.keskn00@gmail.com>
2023-04-09 17:22:38 +0300

    create project

---
+++ CoffeeShop/App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+
+
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
